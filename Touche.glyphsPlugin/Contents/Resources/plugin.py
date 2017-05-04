71f64c966c12c922109ee7c011d8450ae7cd7c8e
schriftgestalt <georg.seifert@mac.com>
2017-05-04 15:41:06 +0200

    use new Plugin API

---
+++ Touche.glyphsPlugin/Contents/Resources/plugin.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+import time, traceback
+
+from GlyphsApp.plugins import *
+
+#import GlyphsApp
+from toucheTool import ToucheTool
+
+class TouchePlugin (GeneralPlugin):
+	def settings(self):
+		self.name = u"Touché"
+	
+	def start(self):
+		try: 
+			newMenuItem = NSMenuItem(self.name, self.showWindow)
+			Glyphs.menu[EDIT_MENU].append(newMenuItem)
+		except:
+			mainMenu = Glyphs.mainMenu()
+			s = objc.selector(self.showWindow, signature='v@:@')
+			newMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(self.name, s, "")
+			newMenuItem.setTarget_(self)
+			mainMenu.itemWithTag_(5).submenu().addItem_(newMenuItem)
+	
+	def showWindow(self, sender):
+		self.touche = ToucheTool()
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
+	
